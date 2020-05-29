63b816aa5f9e29bbbfac6fdcf796b5d933fde329
Victor Maciel <victormaciel0216@gmail.com>
2020-05-29 01:50:09 -0400

    render card text

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -64,7 +64,7 @@ export default class Card extends React.Component {
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                     >
-                        <p>Card Text</p>
+                        <p>{ cardData.text }</p>
                         <EditButton>
                             <FaEdit />
                         </EditButton>
